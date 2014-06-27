235386b5bbec296eb25002c763b588f6e92458f1
James Barwell <jb@jamesbarwell.co.uk>
2014-06-27 15:14:29 +0100

    Use npm requirejs

--- domloader.js
+++ domloader.js
@@ -6,12 +6,21 @@ var doc;
 var window;
 
 function load(html, module, requireOptions, callback) {
-
     if (arguments.length === 3) {
         callback = requireOptions;
         requireOptions = {};
     }
 
+    window = getWindow(html);
+
+    initRequire(requireOptions, function() {
+        window.require([ module ], function(exported) {
+            callback(window, exported);
+        });
+    });
+}
+
+function getWindow(html) {
     html = html || null; // causes basic document to be created
     var level = null; // defaults to 3
     var options = {};
@@ -22,21 +31,18 @@ function load(html, module, requireOptions, callback) {
     // Allow modules to use console to log to STDOUT/ERR
     window.console = console;
 
-    // @todo set require.js options before init
-    window.require = requireOptions;
+    return window;
+}
 
-    // @todo use npm module
-    var requirePath = path.resolve(__dirname, './require.js');
+function initRequire(options, onRequireLoad) {
+    // Set require.js options
+    window.require = options;
+
+    var requirePath = path.resolve(__dirname, './node_modules/requirejs/require.js');
 
     var scriptEl = window.document.createElement("script");
     scriptEl.src = requirePath;
-
-    scriptEl.onload = function() {
-        window.require([ module ], function(exported) {
-            callback(window, exported);
-        });
-    }
-
+    scriptEl.onload = onRequireLoad;
     window.document.body.appendChild(scriptEl);
 }
 
